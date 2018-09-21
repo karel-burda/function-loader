e435962879c1d2c042578a38c9b4bff84c482000
Karel Burda <karel.burda@nxp.com>
2018-09-21 05:27:37 +0200

    Added const-correctness

--- include/function_extractor/detail/function_loader_base_unix.hpp
+++ include/function_extractor/detail/function_loader_base_unix.hpp
@@ -13,7 +13,7 @@ namespace detail
 class function_loader_base
 {
 protected:
-    void * get_function_address(void * handle, const std::string & function_name)
+    void * get_function_address(void * handle, const std::string & function_name) const
     {
         return dlsym(handle, function_name.c_str());
     }
--- include/function_extractor/detail/function_loader_base_windows.hpp
+++ include/function_extractor/detail/function_loader_base_windows.hpp
@@ -13,7 +13,7 @@ namespace detail
 class function_loader_base
 {
 protected:
-    void * get_function_address(HMODULE handle, const std::string & function_name)
+    void * get_function_address(HMODULE handle, const std::string & function_name) const
     {
         return GetProcAddress(handle, function_name.c_str());
     }
