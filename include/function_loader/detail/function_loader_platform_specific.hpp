5550099418b404d711f14428b18a4220b8784f8b
Karel Burda <karel.burda@nxp.com>
2018-11-16 17:05:36 +0100

    Debugging Linux build on the CI

--- include/function_loader/detail/function_loader_platform_specific.hpp
+++ include/function_loader/detail/function_loader_platform_specific.hpp
@@ -2,7 +2,7 @@
 
 #ifdef _WIN32
 #include "function_loader/detail/function_loader_platform_specific_windows.hpp"
-#elif __linux__
+#elif __unix__
 #include "function_loader/detail/function_loader_platform_specific_unix.hpp"
 #else
 #error Platform not implemented
